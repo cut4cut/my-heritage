e2e1925088c6ceb5947991f51e883307dcac00be
Владислав Пруд <vdprud@yandex.ru>
2023-05-07 01:15:00 +0300

    add PastvuAPI class

--- heritage/pkg/pastvu/conn.py
+++ heritage/pkg/pastvu/conn.py
@@ -1,5 +1,4 @@
-
-from dataclasses import dataclass
+from typing import List, Tuple
 
 import orjson
 import httpx
@@ -12,7 +11,7 @@ def orjson_dumps(v, *, default):
 
 
 class GeoPoint(BaseModel):
-    latitude: float = 55.724322
+    latitude: float = 55.824322
     longitude: float = 37.611089
 
 
@@ -20,10 +19,10 @@ class Params(BaseModel):
     geo: GeoPoint = GeoPoint()
     limit: int = 3
     skip: int = 0
-    
+
     def next(self) -> None:
         self.skip += self.limit
-    
+
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
@@ -35,49 +34,87 @@ class Params(BaseModel):
 class Photo(BaseModel):
     geo: GeoPoint
     title: str
+    source: str
+    period: str
     file: str
     cid: int
 
-    class Config:
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
-        json_encoders = {
-            GeoPoint: lambda g: [g.latitude, g.longitude],
-        }
 
+class PastvuAPI:
+    def __init__(self) -> None:
+        self.base_url = "https://pastvu.com/api2"
+
+    def get_photo_info(self, cid: str) -> Tuple[str, str]:
+        r = httpx.get(
+            self.base_url,
+            params={"method": "photo.giveForPage",
+                    "params": f'{{"cid": {cid}}}'},
+        )
+        contents = orjson.loads(r.content).get("result", {}).get("photo", {})
+        return contents.get("source", ""), contents.get("y", "")
+
+    def get_nearest_photos(self, params: Params) -> List[Photo]:
+        r = httpx.get(
+            self.base_url,
+            params={
+                "method": "photo.giveNearestPhotos",
+                "params": params.json(models_as_dict=False),
+            },
+        )
+
+        photos = []
+        for data in orjson.loads(r.content).get("result", {}).get("photos", {}):
+            source, period = self.get_photo_info(data["cid"])
+            photos.append(
+                Photo(
+                    geo=GeoPoint(
+                        latitude=data["geo"][0],
+                        longitude=data["geo"][1]
+                    ),
+                    title=data["title"],
+                    file=data["file"],
+                    cid=data["cid"],
+                    source=source,
+                    period=period,
+                )
+            )
+
+        return photos
 
 
 def main():
-
     p = Params()
     print(p)
 
-    r = httpx.get("https://pastvu.com/api2", params={
-        "method": "photo.giveNearestPhotos",
-        "params": p.json(models_as_dict=False)
-    })
+    r = httpx.get(
+        "https://pastvu.com/api2",
+        params={
+            "method": "photo.giveNearestPhotos",
+            "params": p.json(models_as_dict=False),
+        },
+    )
 
-
-    contents = orjson.loads(r.content) 
+    contents = orjson.loads(r.content)
 
     for idx, data in enumerate(contents.get("result", {}).get("photos", {})):
-        #print(idx, photo)
+        # print(idx, photo)
         photo = Photo(
             geo=GeoPoint(latitude=data["geo"][0], longitude=data["geo"][1]),
             title=data["title"],
             file=data["file"],
-            cid=data["cid"]
+            cid=data["cid"],
         )
         print(photo)
 
-        r = httpx.get("https://pastvu.com/api2", params={
-            "method": "photo.giveForPage",
-            "params": f'{{"cid": {photo.cid}}}'
-        })
-        contents = orjson.loads(r.content) 
+        r = httpx.get(
+            "https://pastvu.com/api2",
+            params={"method": "photo.giveForPage",
+                    "params": f'{{"cid": {photo.cid}}}'},
+        )
+        contents = orjson.loads(r.content).get("result", {}).get("photo", {})
         print(contents)
+        print(contents["source"], contents["y"])
         break
-
 
     print()
 
