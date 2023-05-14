81145c17c4ff58855b57d72ac418dc368a7de631
Владислав Пруд <vdprud@yandex.ru>
2023-05-14 12:02:39 +0300

    add str formating

--- heritage/dto/state.py
+++ heritage/dto/state.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import orjson
 
 
 @dataclass
@@ -6,6 +7,9 @@ class SearchState:
     latitude: float = 55.824322
     longitude: float = 37.611089
     page: int = 1
+
+    def __str__(self) -> str:
+        return orjson.dumps(self).decode("utf-8")
 
     def shift(self) -> None:
         self.page += 1
